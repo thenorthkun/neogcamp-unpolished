6408ebaa9ab353a399e9a783ca5b8b8ab826ce06
Dhruv-VINT <dhruv.rathore99@gmail.com>
2020-12-19 17:54:49 +0530

    URL integrated

--- VanillaJS App/app.js
+++ VanillaJS App/app.js
@@ -13,11 +13,13 @@ var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector('#output')
 
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" // Mock server
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text = " + text
+function getTranslationURL(input){
+    return serverURL + "?" + "text = " + input
 }
 
 function errorHandler(error) {
@@ -26,15 +28,19 @@ function errorHandler(error) {
 }
 
 function clickHandler() {
-    // outputDiv.innerText = "bananananaanananaana  " + txtInput.value;
+
     var inputText = txtInput.value;  //taking input
 
 
     // calling server for processing
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;  //output
+    })
     .catch(errorHandler)
+    
 };
 
 
