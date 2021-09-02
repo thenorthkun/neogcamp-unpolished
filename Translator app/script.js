af759b12d018d88439fee3554848f76474ed58cb
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-09-02 22:45:08 +0530

    fix

--- Translator app/script.js
+++ Translator app/script.js
@@ -1,6 +1,13 @@
-var username = prompt("What the hell"); // Prompt takes input & returns the value taken
-console.log(username)
+// var username = prompt("What the hell"); // Prompt takes input & returns the value taken
+// console.log(username)
 
-var message = alert("you;re a patien"); // Alert displays a direct message on a pop-up
-console.log(message)
+// var message = alert("you;re a patien"); // Alert displays a direct message on a pop-up
+// console.log(message)
 
+var button = document.querySelector("#btn-translate");
+
+function clickhandler(){
+    console.log("Clicked");
+};
+
+button.addEventListener("click", clickhandler)
