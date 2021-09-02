9c1f776d25facf8da88db401cf2482223b743bc1
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-09-02 18:52:05 +0530

    alert & prompt exec

---
+++ Translator app/script.js
@@ -0,0 +1,6 @@
+var username = prompt("What the hell"); // Prompt takes input & returns the value taken
+console.log(username)
+
+var message = alert("you;re a patien"); // Alert displays a direct message on a pop-up
+console.log(message)
+
