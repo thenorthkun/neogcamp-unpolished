97c40db61cbd10d77e2e6e13358e858657f25025
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-09-03 15:00:04 +0530

    progress

--- Translator app/script.js
+++ Translator app/script.js
@@ -4,10 +4,14 @@
 // var message = alert("you;re a patien"); // Alert displays a direct message on a pop-up
 // console.log(message)
 
+var input = document.querySelector("#input");
 var button = document.querySelector("#btn-translate");
+
+console.log(input)
 
 function clickhandler(){
     console.log("Clicked");
+    console.log(input.value)
 };
 
 button.addEventListener("click", clickhandler)
