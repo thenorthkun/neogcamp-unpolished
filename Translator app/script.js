bb0643bd199e60cccc89331725e0194997037d7c
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-09-07 05:19:05 +0530

    web app foundations

--- Translator app/script.js
+++ Translator app/script.js
@@ -1,7 +1,7 @@
-// var username = prompt("What the hell"); // Prompt takes input & returns the value taken
+// var username = prompt("What the hell"); // PROMPT TAKES INPUT & RETURNS THE VALUE TAKEN
 // console.log(username)
 
-// var message = alert("you;re a patien"); // Alert displays a direct message on a pop-up
+// var message = alert("you;re a patien"); // ALERT DISPLAYS A DIRECT MESSAGE ON A POP-UP
 // console.log(message)
 
 var input = document.querySelector("#input");
@@ -25,10 +25,43 @@ var output = document.querySelector("#output");
 //     console.log(input.value);
 // };
 
-function clickhandler(){
-    output.innerText = "This input will be translated : " + input.value;
-    // output.innerText = "This input will be translated : " + input;
+// function clickhandler(){
+//     output.innerText = "This input will be translated : " + input.value;
+//     // output.innerText = "This input will be translated : " + input;
 
+// }
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text){
+    // return serverURL;
+    return serverURL + "?" + "text=" + text;
 }
 
+function errorHandler(error){
+    console.log("An error occured " + error );
+    alert("An unexpected error was caught !");
+     
+}
+function clickhandler(){
+
+    var inputText = input.value; // TAKING INPUT
+
+    // CALLING SERVER FOR PROCESSING
+    fetch(getTranslationURL(inputText)) // RUNS THE FUNCTION WHERE TEXT = INPUTTEXT
+    .then(response =>  response.json())
+    // .then(json => console.log(json))
+    // .then(json => console.log(json.contents.text))
+    // .then(json => console.log(json.contents.translated))
+    .then(json => {
+        // var translatedText = json.contents.text;
+        var translatedText = json.contents.translated;
+        output.innerText = translatedText; // OUTPUT
+    })
+
+
+    .catch(errorHandler) //WHEN THE SERVER IS DOWN OR GETS CAUGHT IN AN ERROR 
+};
+
 button.addEventListener("click", clickhandler)
+
