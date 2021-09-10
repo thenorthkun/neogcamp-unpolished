15a3e12e662e2ad4c181208b80a658987eea7875
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-09-11 02:35:40 +0530

    init

--- Translator app/Demo app/script.js
+++ Translator app/Demo app/script.js
@@ -1,1 +1,23 @@
-console.log(input.value)
+var input = document.querySelector('#input')
+var button = document.querySelector('#btn-translate')
+var output = document.querySelector('#output')
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+
+function getTranslationsURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function clickhandler(){
+    var inputText = input.value;
+
+    fetch(getTranslationsURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var outputText = json.contents.translated;
+        output.innerHTML = outputText;}
+        )
+}
+
+
+button.addEventListener("click", clickhandler)
