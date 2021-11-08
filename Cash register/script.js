d58f27162f8e3e92b2d8b8cf988b3171ccb57d0e
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-11-08 11:38:05 +0530

    fix : center align

---
+++ Cash register/script.js
@@ -0,0 +1,14 @@
+const billAmt = document.querySelector('#billAmount')
+const nextBtn = document.querySelector('#next-btn')
+const cashGvn = document.querySelector('#cashGiven')
+const checkBtn = document.querySelector('#check-btn')
+
+const msg = document.querySelector('#message')
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
+const noOfNotes = document.querySelectorAll('#noOfNotes')
+
+nextBtn.addEventListener('click', function() 
+{
+    
+}
