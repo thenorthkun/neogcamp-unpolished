d2f029009b2f294386ade1931af4f845578ad2e5
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-11-23 03:13:37 +0530

    re-init

--- Cash register/script.js
+++ Cash register/script.js
@@ -5,10 +5,24 @@ const checkBtn = document.querySelector('#check-btn')
 
 const msg = document.querySelector('#message')
 
+const output = document.querySelector('#output')
+const outputDiv = document.querySelector('#box')
+
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 const noOfNotes = document.querySelectorAll('#noOfNotes')
 
-nextBtn.addEventListener('click', function() 
-{
-    
+function showMessage(text){
+    msg.style.display = "block";
+    msg.innerText = text;
+
 }
+
+nextBtn.addEventListener('click', function() {
+    // console.log(billAmt.value)
+    if(billAmt.value > 0){
+        nextBtn.style.display = "none";
+        outputDiv.style.display = "block";
+    }
+    else{
+        showMessage("Enter valid bill amount.")
+    }})
