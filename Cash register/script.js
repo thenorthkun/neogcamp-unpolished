6682f1683612839c76a2f70a7d7e2d0bf7f0a673
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-11-25 15:42:24 +0530

    logic fix 1 ✅

--- Cash register/script.js
+++ Cash register/script.js
@@ -12,11 +12,10 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 const noOfNotes = document.querySelectorAll('#noOfNotes')
 
 
-
 function showMessage(text){
     msg.style.display = "block";
     msg.innerText = text;
-    outputDiv.style.display = "none";
+    // outputDiv.style.display = "none";
 }
 
 function hideMessage(){
@@ -24,27 +23,34 @@ function hideMessage(){
 }
 
 
+// nextBtn.addEventListener('click', function() {
 
-nextBtn.addEventListener('click', function() {
+//     hideMessage()
 
-    hideMessage()
+//     // console.log(billAmt.value)
+//     if(billAmt.value > 0){
+//         nextBtn.style.display = "none";
+//         cashGvn.style.display = "block";
+//     }
+//     else{
+//         showMessage("Enter valid bill amount.")
+//     }
+// })
 
-    // console.log(billAmt.value)
-    if(billAmt.value > 0){
-        nextBtn.style.display = "none";
-        cashGvn.style.display = "block";
-    }
-    else{
-        showMessage("Enter valid bill amount.")
-}})
+
 
 
 checkBtn.addEventListener('click', function(){
 
-    if(billAmt.value>0){
-        if(billAmt.value >= cashGvn.value){
-            const amtToBeReturned = cashGvn.value - billAmt.value;
+    let billAmtValue = Number(billAmt.value)
+    let cashGvnValue = Number(cashGvn.value)
+
+    if(billAmtValue>0){
+        if(billAmtValue <= cashGvnValue){
+            const amtToBeReturned = cashGvnValue - billAmtValue;
             console.log(amtToBeReturned);
+            calculateChange(amtToBeReturned);
+            
         }
         else{
             showMessage("Cash given must be greater than the bill")
@@ -55,3 +61,14 @@ checkBtn.addEventListener('click', function(){
     }
 
 )
+
+function calculateChange(amtToBeReturned) {
+    for(let i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amtToBeReturned/availableNotes[i]);
+
+        amtToBeReturned = amtToBeReturned % availableNotes[i];
+
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
