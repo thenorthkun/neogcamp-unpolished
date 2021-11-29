f1bbbbd179d70493285f1007d8eed429f1d1ff03
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-11-30 05:22:26 +0530

    Q.2 ✅

---
+++ Sample Interview questions/question2/script.js
@@ -0,0 +1,21 @@
+const input = document.querySelector("#input")
+const increase = document.querySelector("#increase")
+const decrease = document.querySelector("#decrease")
+
+increase.addEventListener("click", increaseFunction)
+decrease.addEventListener("click" , decreaseFunction)
+
+var size = 16;
+// var defaultSize = 16;
+
+
+function increaseFunction(){
+    size = size + 2 // 16+2 = 18
+    input.style.fontSize = `${size}px`;
+}
+
+function decreaseFunction(){
+    size = size - 2 //16-2 = 14
+    input.style.fontSize = `${size}px`;
+}
+
