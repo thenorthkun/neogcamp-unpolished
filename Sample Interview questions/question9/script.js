5585822a9062ea1e47f4944416f0d436127d642f
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-12-03 00:53:21 +0530

    logic-fix (Q9)

--- Sample Interview questions/question9/script.js
+++ Sample Interview questions/question9/script.js
@@ -2,14 +2,28 @@ const inputText = document.querySelector('#inputText')
 const button = document.querySelector('#checkBtn')
 const output = document.querySelector('#output')
 
-button.disabled = true;
+// button.disabled = true;
+
+button.addEventListener('click', function(){
+    let value = inputText.value;
+    if(value.length < 10){
+        button.disabled = true;
+        output.innerText = 'Error ❌'
+    }
+    else{
+        button.disabled = false;
+        output.innerText = 'Correct ✔️'
+        inputText.style.color = "green"
+    }
+})
 
 inputText.addEventListener('input', function(){
     let value = inputText.value;
-    if(value.length > 10){
+    if(value.length < 10){
         button.disabled = true;
     }
     else{
         button.disabled = false;
     }
 })
+
