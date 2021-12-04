53e962f2789f7ac2eee7ad051b4c3c4d9a867046
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-12-05 01:58:32 +0530

    6 - X
    Here's an API. Create a web app to call this API with your full name and print the response. This could be extended where we ask you to do something with the response. Like add a text, or capitalise etc.
    
    API link - https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json

--- Rough work/pencil.js
+++ Rough work/pencil.js
@@ -1,6 +1,21 @@
-const text = document.querySelector('#text')
-const button = document.querySelector('#hideBtn')
+const input =  document.querySelector('#input')
+const button = document.querySelector('#callAPI')
+const output = document.querySelector('#output')
+
+var URL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getResponse(text){
+    return URL + '?' + 'text=' + text;
+}
 
 button.addEventListener('click', function(){
-    text.style.display = 'none'
+    let inputName = input.value;
+    fetch(getResponse(inputName))
+    .then(response => response.json())
+    .then(json => {
+        var output1 = json.contents.translated;
+        var output2 = json.contents.text;
+        output.innerText = `${output1} ${output2.toUpperCase()}`
+    })
+
 })
