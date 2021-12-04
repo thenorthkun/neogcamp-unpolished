8d03f978af7d965e78163239e25c165b3ac437b6
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-12-05 02:06:08 +0530

    7 - X
    Here's an API. It will give an error. Write a web app, call this API and read the error message. Show user the error message.
    
    API link - https://BROKENlessonfourapi.tanaypratap.repl.co/translate/yoda.json

--- Rough work/pencil.js
+++ Rough work/pencil.js
@@ -1,21 +1,15 @@
-const input =  document.querySelector('#input')
 const button = document.querySelector('#callAPI')
 const output = document.querySelector('#output')
 
-var URL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
-function getResponse(text){
-    return URL + '?' + 'text=' + text;
-}
+var URL = "https://BROKENlessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 button.addEventListener('click', function(){
-    let inputName = input.value;
-    fetch(getResponse(inputName))
-    .then(response => response.json())
-    .then(json => {
-        var output1 = json.contents.translated;
-        var output2 = json.contents.text;
-        output.innerText = `${output1} ${output2.toUpperCase()}`
-    })
+    fetch(URL)
+    .then(response => console.log(response))
 
+    .catch(errorHandler)
 })
+
+function errorHandler(error){
+    output.innerText = error.message;
+}
