e0fd6991bb7148826b4da80f3126c7bc53478c4e
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-12-05 02:20:33 +0530

    8 - X
    Here's an API, it can give two errors. Either 404, or 401. If the error is 404, show the user 'page not found' and if the error is 401, show the user 'you are not logged in'.
    
    API link - https://reqres.in/api/users/23

--- Rough work/pencil.js
+++ Rough work/pencil.js
@@ -1,15 +1,18 @@
 const button = document.querySelector('#callAPI')
 const output = document.querySelector('#output')
 
-var URL = "https://BROKENlessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var URL = "https://reqres.in/api/users/23"
 
 button.addEventListener('click', function(){
     fetch(URL)
-    .then(response => console.log(response))
 
-    .catch(errorHandler)
+    .then(response => {
+        if(response.status === 404){
+            output.innerText = 'Page not found'
+        }
+        else if(response.status === 401){
+            output.innerText = 'you are not logged in'
+        }
+    })
+    
 })
-
-function errorHandler(error){
-    output.innerText = error.message;
-}
